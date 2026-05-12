c97eabbf8fe00657234474b0500ecdd86dfc31dc
4rthur12 <arthur.fantinelli@escola.pr.gov.br>
2026-05-12 10:39:16 -0300

    a2

--- main.js
+++ main.js
@@ -9,15 +9,3 @@ for (let i = 0; i < botoes.length; i++) {
         botoes[i].classList.add("ativo");
     };
 }
-
-const botoes2 = document.querySelectorAll(".botao2");
-
-for (let i = 0; i < botoes.length; i++) {
-    botoes2[i].onclick = function () {
-        for (let j = 0; j < botoes.length; j++){
-            botoes2[j].classList.remove("ativo");
-        }
-
-        botoes2[i].classList.add("ativo");
-    };
-}
